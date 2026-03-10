1c5d49497d0d169d3e24bb91486a6ccb8be3bd7c
JunjieLi <nicejunjie@gmail.com>
2026-03-09 22:18:32 -0500

    Add nvpl internal blas symbols to incerception list

--- freplace.h
+++ freplace.h
@@ -40,6 +40,37 @@ enum findex{
   zherk,
   cher2k,
   zher2k,
+  sgemm_internal_,
+  dgemm_internal_,
+  cgemm_internal_,
+  zgemm_internal_,
+  ssymm_internal_,
+  dsymm_internal_,
+  csymm_internal_,
+  zsymm_internal_,
+  ssyrk_internal_,
+  dsyrk_internal_,
+  csyrk_internal_,
+  zsyrk_internal_,
+  ssyr2k_internal_,
+  dsyr2k_internal_,
+  csyr2k_internal_,
+  zsyr2k_internal_,
+  strmm_internal_,
+  dtrmm_internal_,
+  ctrmm_internal_,
+  ztrmm_internal_,
+  strsm_internal_,
+  dtrsm_internal_,
+  ctrsm_internal_,
+  ztrsm_internal_,
+  chemm_internal_,
+  zhemm_internal_,
+  cherk_internal_,
+  zherk_internal_,
+  cher2k_internal_,
+  zher2k_internal_
+
 };
 
 #define X(f0_val, f1_val) { f0_val, f1_val, NULL, 0.0, 0.0 }
@@ -74,5 +105,36 @@ enum findex{
     X("zherk_", "myzherk"), \
     X("cher2k_", "mycher2k"), \
     X("zher2k_", "myzher2k"), \
+    X("sgemm_internal_", "mysgemm"), \
+    X("dgemm_internal_", "mydgemm"), \
+    X("cgemm_internal_", "mycgemm"), \
+    X("zgemm_internal_", "myzgemm"), \
+    X("ssymm_internal_", "myssymm"), \
+    X("dsymm_internal_", "mydsymm"), \
+    X("csymm_internal_", "mycsymm"), \
+    X("zsymm_internal_", "myzsymm"), \
+    X("ssyrk_internal_", "myssyrk"), \
+    X("dsyrk_internal_", "mydsyrk"), \
+    X("csyrk_internal_", "mycsyrk"), \
+    X("zsyrk_internal_", "myzsyrk"), \
+    X("ssyr2k_internal_", "myssyr2k"), \
+    X("dsyr2k_internal_", "mydsyr2k"), \
+    X("csyr2k_internal_", "mycsyr2k"), \
+    X("zsyr2k_internal_", "myzsyr2k"), \
+    X("strmm_internal_", "mystrmm"), \
+    X("dtrmm_internal_", "mydtrmm"), \
+    X("ctrmm_internal_", "myctrmm"), \
+    X("ztrmm_internal_", "myztrmm"), \
+    X("strsm_internal_", "mystrsm"), \
+    X("dtrsm_internal_", "mydtrsm"), \
+    X("ctrsm_internal_", "myctrsm"), \
+    X("ztrsm_internal_", "myztrsm"), \
+    X("chemm_internal_", "mychemm"), \
+    X("zhemm_internal_", "myzhemm"), \
+    X("cherk_internal_", "mycherk"), \
+    X("zherk_internal_", "myzherk"), \
+    X("cher2k_internal_", "mycher2k"), \
+    X("zher2k_internal_", "myzher2k"),
+
     // Add more elements as needed
 
